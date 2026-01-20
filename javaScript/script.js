9c50ca6d3d900c84ae9dad146e825a3bfc14265e
shohag <contact.devshohag@gmail.com>
2026-01-20 14:27:39 +0600

    styling on desktop screen ✅

---
+++ javaScript/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll(".videoWrapper").forEach(wrapper => {
+    wrapper.addEventListener(
+      "click",
+      () => {
+        const videoId = wrapper.dataset.videoId;
+
+        wrapper.classList.add("is-playing");
+
+        wrapper.innerHTML = `
+          <iframe
+            src="https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0"
+            frameborder="0"
+            allow="autoplay; encrypted-media; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        `;
+      },
+      { once: true } // IMPORTANT: click runs only once
+    );
+  });
